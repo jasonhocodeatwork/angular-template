649e41f3f5c515b94fa58cf0ee053b17a02dab27
JasonHo <jasonhocodeatwork@gmail.com>
2022-09-12 13:46:21 +0800

    t1

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.mode';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('r1', 
+    'fuckfuckfuckfuckfuckfuckfuckfuckfuckfuckfuckfuckfuckfuck', 
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLKioLxDBnCPkGzpjl04c7pBNu_5HzypJ5d-hqEWSz&s'),
+    new Recipe('r2', 
+    'fuck2 fuck2 fuck2 fuck2 fuck2', 
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLKioLxDBnCPkGzpjl04c7pBNu_5HzypJ5d-hqEWSz&s')
+  ]
+
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+ 
+
 
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ingredient } from '../shared/ingredient.mode';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+  ingredients: ingredient[] = [new ingredient('apple', 5), new ingredient('banana', 2)]
   constructor() { }
 
   ngOnInit(): void {
