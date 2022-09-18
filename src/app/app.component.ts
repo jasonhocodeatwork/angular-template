d1ecc48ac78f23fc10fc9c7c739eacbaa35721ee
JasonHo <jasonhocodeatwork@gmail.com>
2022-09-18 22:25:11 +0800

    c2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  defaultQ ="pet"
+  answer=""
+  genders = ['male', 'female']
+  @ViewChild("f") form: NgForm
+
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  }
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.form.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: 'abcd',
+    //   gender: 'female'
+    // })
+
+    // only update what you need 
+    this.form.form.patchValue({
+      userData: {
+        username: 'xxxaaa'
+      }
+    })
+  }
+
+  // onsubmit(form: NgForm) {
+  //   console.log(form);
+  // }
+
+  // alternative
+  
+  onsubmit() {
+    this.user.username = this.form.value.userData.username
+    this.user.email = this.form.value.userData.email
+    this.user.secretQuestion = this.form.value.secret
+    this.user.answer = this.form.value.questionAnswer
+    this.user.gender = this.form.value.gender
+    this.form.reset();
   }
 }
